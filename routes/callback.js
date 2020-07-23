1ea4caef7d3547b7f6c94839d8bd64af59f1a530
Dermot O'Brien <contact@dermotobrien.me>
2020-07-23 17:11:40 +0100

    Callback Headers
    Set callback variables as headers for application to intake

--- routes/callback.js
+++ routes/callback.js
@@ -8,9 +8,15 @@ router.get('/spotify', (req, res) => {
   // var storedState = req.cookies ? req.cookies[process.env.SPOTIFY_STATE_KEY] : null;
   // if (state === null || state !== storedState) return res.status(400).json({ message: "Error: State mismatch." });
   res.clearCookie(process.env.SPOTIFY_STATE_KEY);
-
+  try {
   Spotify.authorizeUser(code).then(({ email, display_name, product, country, spotifyId, expires_in, access_token, refresh_token }) => {
     User.generateJWT('Spotify', email, access_token, refresh_token, spotifyId, product, display_name).then(({ token, sid }) => {
+      res.set({
+        'Auth-JWT': token,
+        'Auth-SessionId': sid,
+        'Auth-DisplayName': display_name,
+        'Auth-Email': email
+      });
       res.status(200).json({ jwt: token, email, display_name, sid });
     }).catch((e) => {
       res.status(500).json({ status: 500, message: "Error generating JWT.", e });
@@ -18,6 +24,9 @@ router.get('/spotify', (req, res) => {
   }).catch((details) => {
     res.send({ status: 500, message: "An internal error has occurred.", details, location: "Spotify Authorize User"});
   });
+} catch(err) {
+  res.send({ statusL500, err })
+}
 });
 
 module.exports = router;
