0a7d234f2876b253a15a51216b131dc2bfadac92
Dermot O'Brien <contact@dermotobrien.me>
2020-11-25 19:24:22 +0000

    Changes to Spotify sign-in auth - Access token passed as a GET parameter - Token is verified

--- functions/routes/authentication.js
+++ functions/routes/authentication.js
@@ -20,19 +20,73 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/spotify', async (req, res, next) => {
-  const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 'user-read-email', 'user-read-private', 'user-library-modify', 'user-library-read'],
-    state = generateRandomString(16),
-    stateKey = process.env.SPOTIFY_STATE_KEY,
-    spotifyApi = new SpotifyWebApi({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      redirectUri: process.env.SPOTIFY_REDIRECT_URI
-    });
-  res.cookie(stateKey, global.userId);
-  var authorizeURL = spotifyApi.createAuthorizeURL(scopes, global.userId);
-  // res.json({ authorizeURL })
-  res.redirect(authorizeURL);
+  const {
+    token
+  } = req.query;
+  if (!token) return res.status(401).json({
+    status: 401,
+    message: "Access token is missing."
+  });
+
+  global.admin.auth().verifyIdToken(token).then((user) => {
+    const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 'user-read-email', 'user-read-private', 'user-library-modify', 'user-library-read'],
+      state = generateRandomString(16),
+      stateKey = process.env.SPOTIFY_STATE_KEY,
+      spotifyApi = new SpotifyWebApi({
+        clientId: process.env.SPOTIFY_CLIENT_ID,
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+        redirectUri: process.env.SPOTIFY_REDIRECT_URI
+      });
+    const userId = user.uid;
+    console.log({user})
+    // console.log(userId)
+    res.cookie(stateKey, userId);
+    var authorizeURL = spotifyApi.createAuthorizeURL(scopes, userId);
+    res.redirect(authorizeURL);
+  }).catch((error) => {
+    res.send(error);
+  })
 });
+
+
+// global.admin.auth().verifyIdToken(token)
+// .then((user) => {
+//   const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 'user-read-email', 'user-read-private', 'user-library-modify', 'user-library-read'],
+//     state = generateRandomString(16),
+//     stateKey = process.env.SPOTIFY_STATE_KEY,
+//     spotifyApi = new SpotifyWebApi({
+//       clientId: process.env.SPOTIFY_CLIENT_ID,
+//       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+//       redirectUri: process.env.SPOTIFY_REDIRECT_URI
+//     });
+//   const {
+//     uid
+//   } = user;
+//   res.cookie(stateKey, uid);
+//   var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+//   res.redirect(authorizeURL);
+// }).catch((error) => {
+//   return res.status(403).json({
+//     status: 403,
+//     message: "Invalid access token has been provided",
+//     error
+//   })
+// });
+
+// router.get('/spotify', async (req, res, next) => {
+//   const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 'user-read-email', 'user-read-private', 'user-library-modify', 'user-library-read'],
+//     state = generateRandomString(16),
+//     stateKey = process.env.SPOTIFY_STATE_KEY,
+//     spotifyApi = new SpotifyWebApi({
+//       clientId: process.env.SPOTIFY_CLIENT_ID,
+//       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+//       redirectUri: process.env.SPOTIFY_REDIRECT_URI
+//     });
+//   res.cookie(stateKey, global.userId);
+//   var authorizeURL = spotifyApi.createAuthorizeURL(scopes, global.userId);
+//   // res.json({ authorizeURL })
+//   res.redirect(authorizeURL);
+// });
 
 
 module.exports = router;
