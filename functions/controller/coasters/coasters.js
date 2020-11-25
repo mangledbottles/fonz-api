66eea70de028755dd999d303d8bb655e83de7815
Dermot O'Brien <contact@dermotobrien.me>
2020-11-25 19:27:52 +0000

    Revert "Revert "New checkCoaster get method""
    This reverts commit 6eabf5211fae69090c4a0ff8fbc669103a5665a1.

--- functions/controller/coasters/coasters.js
+++ functions/controller/coasters/coasters.js
@@ -46,6 +46,48 @@ exports.getCoaster = (coasterId) => {
     })
 }
 
+// new endpoint to help error checking.
+// should all of them be status 404 or can we do differnt numbers? 
+// exports.checkCoaster = (coasterId) => {
+//     return new Promise(async (resolve, reject) => {
+//         const coaster = await global.CoastersDB
+//             .doc(coasterId)
+//             .get();
+//         if (!coaster.exists) return reject({
+//             status: 404,
+//             message: 'This coaster is not licensed with Fonz Music.'
+//         });
+//         const {
+//             userId,
+//             active,
+//             paused,
+//             name
+//         } = coaster.data();
+//         console.log({ userId, globalUserId: global.userId})
+//         if (userId !== global.userId && userId !== null) return reject({
+// // can we get the NAME of the userID? 
+// // can we get the NAME of the coaster?
+//             // we can do:
+// // 'this is Jay Cowan's coaster named Philip" (i can build a notification into this in the future
+//             status: 404,
+//             message: 'This coaster is linked to a different Fonz account.'
+//         });
+//          if (userId == global.userId && userId !== null) return reject({
+//              // can we get the NAME of the coaster?
+//             // we can do:
+// // 'this coaster is already linked to your account and named Philip" (i can build a notification into this in the future
+//             status: 404,
+//             message: 'This coaster is already linked to your account.'
+//         });
+//         resolve({
+//             name,
+//             paused,
+//             active,
+//             userId
+//         });
+//     })
+// }
+
 exports.addCoasterToAccount = (coasterId) => {
     return new Promise(async (resolve, reject) => {
         try {
