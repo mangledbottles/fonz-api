d2ef7cf5c926fce46218957a4beb882bd0e29e0f
Dermot <contact@dermotobrien.me>
2021-05-27 09:31:21 +0100

    SQL commands

--- sql/Fonz AWS RDS DB.session.sql
+++ sql/Fonz AWS RDS DB.session.sql
@@ -3,4 +3,33 @@
 SELECT * FROM Users
 
 --@block Update users table
-desc Users
+ALTER TABLE Users
+ALTER emailVerified SET DEFAULT 0;
+-- MODIFY COLUMN emailVerified BOOLEAN;
+-- ADD createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP();
+-- ADD lastSignedInAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP();
+
+--@block Get users DESC
+DESC Users
+
+--@block Create providers TABLE
+CREATE TABLE providers (
+    `userId` INT NOT NULL,
+    `provider` TEXT NOT NULL,
+    `rawId` INT NOT NULL,
+    `email` VARCHAR(255),
+    `displayName` VARCHAR(255)
+);
+
+-- @block Get providers
+SELECT * FROM Providers
+-- desc providers
+
+-- @block Add user provider
+INSERT INTO Providers VALUES (
+    1,
+    "GOOGLE",
+    98765,
+    "contact@dermotobrien.me",
+    "Dermot"
+);
