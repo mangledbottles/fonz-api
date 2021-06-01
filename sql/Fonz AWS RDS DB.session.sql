8d7cfb821629b1d24187f64064178338ca48bad0
Dermot <contact@dermotobrien.me>
2021-06-01 18:52:52 +0100

    SQL for Table management

--- sql/Fonz AWS RDS DB.session.sql
+++ sql/Fonz AWS RDS DB.session.sql
@@ -6,26 +6,40 @@ SELECT * FROM Users
 INSERT INTO Users (email, password) 
 VALUES ("benji@fonzmusic.com", "password")
 
+--@block Fix Users TABLE
+UPDATE Users 
+-- SET userId = '7op26c4l5NernSR56LFak4GTmbz1'
+-- WHERE email = 'didi@fonzmusic.com'
+SET agreedConsent = 1, agreedMarketing = 1;
 
---{
---   "localId": "0aovUNdpi2OeCluTmG2UajZzLtt1",
---   "email": "king@gmail.com",
---   "emailVerified": false,
---   "passwordHash": "nSIBhAy142yEsL2XifEMhMbHnXztEuIFC20Jh+9tjRcQ6IJTd6I02tycRbuOj5lepwcCGAk+KEeD2HyWB59O5w==",
---   "salt": "2Q1chyYAHya6ww==",
---   "lastSignedInAt": "1604080619805",
---   "createdAt": "1604080619805",
---   "providerUserInfo": []
--- },
 
 --@block Update users table
 ALTER TABLE Users
-ADD passwordSalt VARCHAR(100) AFTER password;
--- ADD userId VARCHAR(28) NOT NULL; 
+ADD agreedConsent TINYINT NOT NULL,
+ADD agreedMarketing TINYINT NOT NULL;
+
+
+-- DROP COLUMN id;
+-- DROP PRIMARY KEY,
+-- ADD PRIMARY KEY (userId);
+-- ADD UNIQUE (userId);
+
+-- MODIFY COLUMN userId SET UNIQUE;
+-- ADD userId VARCHAR(28) NOT NULL FIRST;
+-- CHANGE COLUMN userId userId NOT NULL VARCHAR(28) AFTER id;
+-- ADD passwordSalt VARCHAR(100) AFTER password;
+-- ADD userId VARCHAR(28) NOT NULL DEFAULT 'NA'
 -- ALTER emailVerified SET DEFAULT 0;
 -- MODIFY COLUMN emailVerified BOOLEAN;
 -- ADD createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP();
 -- ADD lastSignedInAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP();
+
+
+--@block Debug Primary Key
+SELECT userId,COUNT(userId)
+FROM Users
+GROUP BY userId
+HAVING COUNT(userId) >1
 
 --@block Get users DESC
 DESC Users
@@ -50,4 +64,27 @@ INSERT INTO Providers VALUES (
     98765,
     "contact@dermotobrien.me",
     "Dermot"
+);
+
+--@block List all Tables
+SHOW Tables
+
+--@block Create Sessions Table
+
+--@block Create providers TABLE
+CREATE TABLE Sessions (
+    `sessionId` VARCHAR(28) NOT NULL,
+    `userId` INT NOT NULL,
+    `status` TINYINT DEFAULT 1,
+    `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    `provider` TEXT NOT NULL
+);
+
+--@block Desc Sessions
+DESC Sessions;
+
+--@block Insert into Sessions
+INSERT INTO Sessions VALUES (
+    'UROqkPBzZ1U3YWZfp6HO2AwpkEh1',
+    ''
 );
