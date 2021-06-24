acdf6934dd57905672af1386a84621d87f36678c
Dermot <contact@dermotobrien.me>
2021-06-24 18:29:05 +0100

    Create Auth.ts

---
+++ controller/Auth.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+import { connect } from '../database/config';
+import { User } from '../database/entity/User';
+
+exports.signIn = (username: string, password: string) => {
+    console.log("controller called")
+    return new Promise(async (resolve, reject) => {
+        try {
+            console.log("trying db")
+            const connection = await connect();
+            const repo = await connection.getRepository(User);
+            console.log({ repo })
+    
+
+            const allUsers = await repo.find();
+            console.log({ allUsers })
+
+            console.log("none")
+            
+            resolve(allUsers);
+            
+
+
+        } catch(error) {
+            console.log(error + "ERROR`")
+            reject(error);
+        }
+    })
+}
