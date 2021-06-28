71fd57eed39d9aec22db1065b1c118446c6241a5
Dermot <contact@dermotobrien.me>
2021-06-28 22:21:51 +0100

    Working with data

--- controller/Auth.ts
+++ controller/Auth.ts
@@ -1,7 +1,10 @@
 'use strict';
 
 import { connect } from '../database/config';
-import { User } from '../entity/User';
+// import { Coasters } from '../entity/Coaster';
+// import { Users } from '../entity/Users'; 
+import { MusicProviders } from '../entity/MusicProviders'; 
+
 
 exports.signIn = (username: string, password: string) => {
     console.log("controller called")
@@ -9,16 +12,29 @@ exports.signIn = (username: string, password: string) => {
         try {
             console.log("trying db")
             const connection = await connect();
-            const repo = await connection.getRepository(User);
-            console.log({ repo })
-            const allUsers = await repo.find();
-            console.log({ allUsers })
-            console.log("none")            
-            resolve(allUsers);
+
+            let MusicProv = new MusicProviders();
+            MusicProv.userId = "509f4484-6830-4f17-aac9-707ccef96fae";
+            const saved = await connection.manager.save(MusicProv);
+
+            resolve(saved);
+
+            // let User = new Users();
+            // User.email = "DERMOT@test.com";
+            // console.log(User)
+            // const saved = await connection.manager.save(User);
+            // resolve(saved);
+
+            // const repo = await connection.getRepository(Users);
+            // const allUsers = await repo.find(); // get all users
+            // console.log({ allUsers })
+            // resolve(allUsers);
             
         } catch(error) {
-            console.log(error + "ERROR`")
-            reject(error);
+            console.error(error)
+            // console.log(error + "ERROR`")
+            // reject(error);
+            reject({})
         }
     })
 }
