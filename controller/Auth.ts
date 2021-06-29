b4f7dd8691379d3862cbc52271e3b6c44cd67a7e
Dermot <contact@dermotobrien.me>
2021-06-29 13:00:54 +0100

    Get Coasters Function
    Wrong location, will move

--- controller/Auth.ts
+++ controller/Auth.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import { connect } from '../database/config';
-// import { Coasters } from '../entity/Coaster';
+import { Coasters } from '../entity/Coasters';
 // import { Users } from '../entity/Users'; 
 import { MusicProviders } from '../entity/MusicProviders'; 
 
@@ -14,13 +14,14 @@ exports.signIn = (username: string, password: string) => {
             const connection = await connect();
 
             let MusicProv = new MusicProviders();
-            MusicProv.userId = "509f4484-6830-4f17-aac9-707ccef96fae";
+            // MusicProv.userId = "509f4484-6830-4f17-aac9-707ccef96fae";
+            MusicProv.userId = "3acaa0e4-fa32-46ee-a17b-50d9a08665d4";
             const saved = await connection.manager.save(MusicProv);
-
             resolve(saved);
 
+
             // let User = new Users();
-            // User.email = "DERMOT@test.com";
+            // User.email = "ROHAN@test.com";
             // console.log(User)
             // const saved = await connection.manager.save(User);
             // resolve(saved);
@@ -34,6 +35,23 @@ exports.signIn = (username: string, password: string) => {
             console.error(error)
             // console.log(error + "ERROR`")
             // reject(error);
+            reject({})
+        }
+    })
+}
+
+exports.getCoasters = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const connection = await connect();
+            const repo = connection.getRepository(Coasters);
+            let coasters = await repo.find();
+
+            console.log({ coasters })
+            resolve({ coasters })
+
+        } catch(error) {
+            console.error(error);
             reject({})
         }
     })
