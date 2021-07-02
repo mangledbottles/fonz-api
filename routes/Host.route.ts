0fd8dc21185aeb95ea45b5c8b3f4019044b56716
Dermot <contact@dermotobrien.me>
2021-07-02 14:39:52 +0100

    Implement Coaster route in Host Route

--- routes/Host.route.ts
+++ routes/Host.route.ts
@@ -1,14 +1,14 @@
-import express, { IRouter, Request, Response } from "express";
+import express, { IRouter, Request, Response, Router } from "express";
 var router: IRouter = express.Router();
 
 /** Import routers under /host */
-// const CoasterRoutes: Router = require('./Coasters.route');
+const CoasterRoutes: Router = require('./Coasters.route');
 
 router.get('/', (req: Request, res: Response) => {
     res.send({ message: "HOST AUTHENTICATED"})
 })
 
 /* Coasters */
-// router.use('/coasters', CoasterRoutes);
+router.use('/coasters', CoasterRoutes);
 
 module.exports = router;
