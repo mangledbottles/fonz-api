d3831379a69e750ca7a0fbe0b5694f6f0a7351a4
Dermot <contact@dermotobrien.me>
2021-07-02 13:56:22 +0100

    Delete index.ts

--- routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express, { IRouter, Request, Response } from "express";
-var router: IRouter = express.Router();
-
-router.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ status: 200, message: "Fonzi API Server Active." })
-});
-
-module.exports = router;
