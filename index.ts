075a989476e55e804bacad00eed647f5ca313a8f
Dermot <contact@dermotobrien.me>
2021-07-02 13:54:52 +0100

    Auth Checker and New Routing

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import express, { Application, NextFunction, Request, Response, Router } from "express";
+
+/* Import dependecies */
+const jwt = require("jsonwebtoken");
 var cookieParser = require('cookie-parser');
 var dotenv = require('dotenv');
 dotenv.config();
@@ -6,13 +9,45 @@ const bodyParser = require('body-parser');
 const rateLimit = require("express-rate-limit");
 const logger = require('morgan');
 
+// const AuthChecker = (req: Request, res: Response, next: NextFunction) => {
+const AuthChecker = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) return res.status(401).json({
+            status: 401,
+            message: "Authentication token missing"
+        });
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+            if(err) {
+                let resp;
+                switch(err.message) {
+                    case 'invalid signature':
+                        resp = { message: "Authentication not valid." }
+                        break;
+                    case 'jwt expired':
+                        resp = { message: "Authentication token expired.", expiredAt: err.expiredAt }
+                        break;
+                 
+                    default:
+                        
+
+                }
+                res.status(401).send(resp);
+            }
+            next();
+        });
+               
+    } catch (err) {
+        res.status(500).json({
+            message: "Could not authenticate."
+        });
+    }
+}
+
+/* Initialise API Application and Port */
 const app: Application = express();
 const port: string = process.env.PORT;
-const limiter = rateLimit({
-    windowMs: process.env.LIMITER_TIMEOUT_RESET,
-    max: process.env.LIMITER_MAX_REQUESTS
-});
-app.use(limiter);
 
 // Remove X-Powered-By Express and add custom header
 app.use((req: Request, res: Response, next: NextFunction) => {
@@ -20,6 +55,11 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader('A-PWNER-MESSAGE', 'VGhpcyBpcyBhIHByaXZhdGUgQVBJClVuYXV0aG9yaXNlZCB1c2Ugd2lsbCBiZSBkZXRlY3RlZCwgYW5kIHdlIHdpbGwgZmluZCB5b3UsIHdhdGNoIG91dC4=')
     next();
 })
+const limiter = rateLimit({
+    windowMs: process.env.LIMITER_TIMEOUT_RESET,
+    max: process.env.LIMITER_MAX_REQUESTS
+});
+app.use(limiter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.json())
@@ -28,20 +68,21 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-// Routes
-const IndexRoute: Router = require('./routes/index.ts');
-const AuthenticationRoute: Router = require('./routes/authentication.ts');
+/* Routes */
+const IndexRoute: Router = require('./routes/Index.route');
+const AuthenticationRoute: Router = require('./routes/Auth.route');
+const HostRoute: Router = require('./routes/Host.route');
 
-/* Requests don't require authentication */
+/** Requests don't require authentication */
 app.use('/', IndexRoute);
-// app.use('/callback', callbackRouter);
 app.use('/auth', AuthenticationRoute);
+// app.use('/callback', callbackRouter);
 
-// /** All requests after this require authentication */
-// app.use(authChecker);
+/** All requests after this require authentication */
+app.use(AuthChecker);
+app.use('/host', HostRoute);
 // app.use('/guest', guestRouter);
 // app.use('/library', libraryRouter);
-// app.use('/host', hostRouter);
 
 /** All unknown URL requests managed here */
 app.use((req: Request, res: Response) => {
