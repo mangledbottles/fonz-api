4d505751ebb81aff8956b08fa23d4d13554dcd6a
Dermot <contact@dermotobrien.me>
2021-07-02 14:39:38 +0100

    Get Coasters route

--- routes/Coasters.route.ts
+++ routes/Coasters.route.ts
@@ -2,8 +2,14 @@ import express, { IRouter, Request, Response } from "express";
 var router: IRouter = express.Router();
 
 /** Import controllers */
-// const Coaster = require('../controller/Coasters.controller');
+const Coaster = require('../controller/Coasters.controller');
 
 router.get('/', (req: Request, res: Response) => {
-    res.send({ message: 1234 })
+    Coaster.getCoasters(res.locals.userId).then((coasters) => {
+        res.send({ coasters, quantity: coasters.length });
+    }).catch((error) => {
+        res.status(error.status || 500).send(error)
+    })
 });
+
+module.exports = router;
