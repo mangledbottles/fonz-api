9a5a389ab85feeec750b9dc1286aa85bf4342619
Dermot <contact@dermotobrien.me>
2021-07-06 18:04:16 +0100

    Updated JWT verifier

---
+++ middleware/extractJWT.ts
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+// import config from '../config/config';
+import logging from '../config/logging';
+import { Request, Response, NextFunction } from 'express';
+
+const NAMESPACE = 'Auth';
+
+const extractJWT = (req: Request, res: Response, next: NextFunction) => {
+    logging.info(NAMESPACE, 'Validating token');
+
+    let token = req.headers.authorization?.split(' ')[1];
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_PRIVATE_KEY, (error, decoded) => {
+            if(error) {
+                let resp = { message: "Authentication could not be completed."}
+                switch(error.message) {
+                    case 'invalid signature':
+                        resp = { message: "Authentication not valid, invalid signature." }
+                        break;
+                    case 'jwt expired':
+                        resp = { message: "Authentication token expired." }
+                        break;
+                }
+                res.status(401).send(resp);
+            }
+
+            const { userId } = decoded;
+            res.locals.userId = userId;
+            next();
+        });
+    } else {
+        res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+};
+
+export default extractJWT;
