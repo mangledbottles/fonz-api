91ec8069766977a90161e263d3c1637aa1162d18
Dermot <contact@dermotobrien.me>
2021-07-19 13:32:44 +0100

    Update get all coasters and create get individual coaster route

--- routes/Coasters.route.ts
+++ routes/Coasters.route.ts
@@ -6,7 +6,16 @@ const Coaster = require('../controller/Coasters.controller');
 
 router.get('/', (req: Request, res: Response) => {
     Coaster.getCoasters(res.locals.userId).then((coasters) => {
-        res.send({ coasters, quantity: coasters.length });
+        res.send(coasters);
+    }).catch((error) => {
+        res.status(error.status || 500).send(error)
+    })
+});
+
+router.get('/:coasterId', (req: Request, res: Response) => {
+    const { coasterId } = req.params;
+    Coaster.getCoastersById(res.locals.userId, coasterId).then((coaster) => {
+        res.send(coaster);
     }).catch((error) => {
         res.status(error.status || 500).send(error)
     })
