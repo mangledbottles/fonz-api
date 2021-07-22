76524777f6ead2d1005134f0d220c4adfbbf5220
Dermot <contact@dermotobrien.me>
2021-07-22 10:09:14 +0100

    Spotify Guest Middleware for Sessions

--- routes/Spotify.router.ts
+++ routes/Spotify.router.ts
@@ -1,14 +1,22 @@
-import express, { IRouter, Request, Response } from "express";
+import express, { IRouter, NextFunction, Request, Response } from "express";
 var router: IRouter = express.Router();
 
 /** Import controllers */
-// const Session = require('../controller/Sessions.controller');
+const Session = require('../controller/Sessions.controller');
 
 /** Middleware Function to Validate Session */
-async function ValidSession(req: Request, res: Response, next: NextFunction) {
-    
-    next();    
-}
+router.use(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const sessionId = req.baseUrl?.split('/')[2];
+        const session = await Session.getSessionForGuest(sessionId);
+
+        console.log({ session })
+
+        next();    
+    } catch(error) {
+        res.status(error.status || 500).json(error);
+    }
+});
 
 router.get('/', (req: Request, res: Response) => {
     res.send({ message: "Guest Router"})
