9dd2c139ab1473d29b5beb48efe6b90eb0a2bd62
Dermot <contact@dermotobrien.me>
2021-07-25 21:24:14 +0100

    Remove redundant code from Spotify Callback

--- routes/Callback.route.ts
+++ routes/Callback.route.ts
@@ -7,15 +7,8 @@ const Spotify = require('../controller/Spotify.controller');
 
 router.get('/spotify', async (req: Request, res: Response) => {
     try {
-        const { code, state } = req.query,
-            storedStateUserId = req.cookies ? req.cookies[process.env.SPOTIFY_STATE_KEY] : null;
-        // if (storedStateUserId == null) res.status(400).json({
-        //     message: "Error: State mismatch."
-        // });
-        console.log(storedStateUserId)
+        const { code, state } = req.query;
         
-        // res.clearCookie(process.env.SPOTIFY_STATE_KEY);
-
         const { email, display_name, product, country, spotifyId, 
             expires_in, access_token, refresh_token } = await Spotify.authorizeUser(code);
         
@@ -25,10 +18,10 @@ router.get('/spotify', async (req: Request, res: Response) => {
         /** Requirement for Hosts only
         if(product != "premium") res.status().json({
             message: "Spotify Premium is required to stream music."
-        })
-        */
+        }) */
+
+       // TODO: Android Redirect
         res.redirect("fonz-music://spotify")
-        // res.send({ message: "Successfully added Spotify to your account."})
     } catch (error) {
         console.error(error);
         res.status(error.status || 500).send(error);
