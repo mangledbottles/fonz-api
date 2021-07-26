85a7d85472734781a331d6d7c6bc1f1dc00f1009
Dermot <contact@dermotobrien.me>
2021-07-26 18:49:26 +0100

    Remove redundant code from Spotify Controller

--- controller/Spotify.controller.ts
+++ controller/Spotify.controller.ts
@@ -16,30 +16,6 @@ const spotifyApi = new SpotifyWebApi({
   redirectUri: process.env.SPOTIFY_REDIRECT_URI
 });
 
-// function initSpotify() {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       // if (globalThis.Spotify.lastUpdated == undefined) {
-//       //   const provider = await Session.getProvider(globalThis._sessionId);
-//       // //   globalThis.Spotify.provider 
-//       //   globalThis.Spotify.lastUpdated = provider.lastUpdated;
-//       // }
-
-//       spotifyApi.setAccessToken(globalThis.Spotify.access_token);
-//       spotifyApi.setRefreshToken(globalThis.Spotify.refresh_token);
-
-//       // if (((globalThis.lastUpdated + 3600) * 1000) >= parseInt(+new Date()) / 1000) {
-//       //   console.log("Spotify Token being refreshed")
-//       //   const updateRef = await refreshAccessToken();
-//       // }
-
-//       resolve({});
-//     } catch (error) {
-//       console.error(error);
-//       reject(error);
-//     }
-//   });
-// }
 function initSpotify() {
   return new Promise(async (resolve, reject) => {
     try {
