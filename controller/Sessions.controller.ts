56635706db6a0191dcee01e7d473a90a4a126115
Dermot <contact@dermotobrien.me>
2021-07-27 19:35:33 +0100

    Session CRUD
    Create Session
    Update Session
    Get Session by ID
    Get all Sessions
    Remove Session

--- controller/Sessions.controller.ts
+++ controller/Sessions.controller.ts
@@ -62,3 +62,119 @@ exports.getProviderForGuest = (sessionId) => {
         }
     })
 }
+
+
+exports.createSession = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const connection = await connect();
+            const sessionRepo = connection.getRepository(Session);
+
+            const userId = globalThis.userId;
+            let session = new Session();
+
+            session.userId = userId;
+            resolve(await sessionRepo.save(session));
+
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+exports.getSessionById = (sessionId: string) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const connection = await connect();
+            const sessionRepo = connection.getRepository(Session);
+
+            const userId = globalThis.userId;
+            const session = await sessionRepo.findOne({ where: { userId, sessionId } })
+            if (!session) reject({ status: 403, message: "No active sessions" });
+
+            resolve(session);
+
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+exports.getAllSessions = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const connection = await connect();
+            const sessionRepo = connection.getRepository(Session);
+
+            const userId = globalThis.userId;
+            const session = await sessionRepo.find({ where: { userId } })
+            if (!session) reject({ status: 403, message: "No sessions linked to this user" });
+
+            resolve(session);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+exports.deleteSession = (sessionId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const connection = await connect();
+            const sessionRepo = connection.getRepository(Session);
+
+            const userId = globalThis.userId;
+            const session = await sessionRepo.findOne({ where: { userId, sessionId } })
+            if (!session) reject({ status: 403, message: "Session does not exist." });
+
+            await sessionRepo.delete(session);
+
+            resolve({});
+
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+exports.updateSession = (sessionId: string, active?: boolean, providerId?: string) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const connection = await connect();
+            const sessionRepo = connection.getRepository(Session);
+            const musicProviderRepo = connection.getRepository(MusicProviders);
+
+            const userId = globalThis.userId;
+            const session = await sessionRepo.findOne({ where: { userId, sessionId } })
+            if (!session) reject({ status: 403, message: "No active session / session does not exist" });
+
+            console.log({ providerId, active, session })
+
+            /** If requested, update Session status */
+            if (active != undefined) {
+                console.log("UPDATE STATUS")
+                // Save Session
+                session.active = active;
+                await sessionRepo.save(session);
+            }
+
+            /** If requested, music provider will be linked to Session */
+            if (providerId != undefined) {
+                console.log("UPDATE PROVIDER")
+
+                const musicProvider = await musicProviderRepo.findOne({ where: { providerId } });
+                if(!musicProvider) reject({ status: 404, message: "Music provider does not exist"});
+                musicProvider.sessionId = sessionId;
+
+                // Save Music Provider ans Session
+                session.provider = musicProvider.provider;
+                await sessionRepo.save(session);
+                await musicProviderRepo.save(musicProvider);
+            }
+
+            resolve({ message: 'Session has been updated', session })
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
