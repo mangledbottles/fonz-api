6e5135bfee666cc982fd26a082bc081c672cca9f
Dermot <contact@dermotobrien.me>
2021-07-27 19:41:21 +0100

    Get all Music Providers

--- routes/MusicProviders.route.ts
+++ routes/MusicProviders.route.ts
@@ -4,6 +4,19 @@ var router: IRouter = express.Router();
 /** Import dependecies */
 var SpotifyWebApi = require('spotify-web-api-node');
 
+/** Import controllers */
+const Host = require('../controller/Host.controller');
+
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const musicProviders = await Host.getMusicProviders();
+        res.send(musicProviders);
+    } catch (error) {
+        console.error(error);
+        res.status(error.status || 500).send(error);
+    }
+})
+
 router.get('/spotify', async (req: Request, res: Response) => {
     try {
         const scopes = ['user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing', 'app-remote-control', 'streaming', 'user-read-email', 'user-read-private', 'user-library-modify', 'user-library-read'],
@@ -12,12 +25,8 @@ router.get('/spotify', async (req: Request, res: Response) => {
                 clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
                 redirectUri: process.env.SPOTIFY_REDIRECT_URI
             });
-
-        // res.cookie(stateKey, res.locals.userId);
         const authorizeURL = spotifyApi.createAuthorizeURL(scopes, res.locals.userId);
         res.send({ authorizeURL })
-        // res.redirect(authorizeURL);
-
     } catch (error) {
         console.error(error);
         res.status(error.status || 500).send(error);
