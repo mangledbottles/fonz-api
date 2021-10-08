dcf202bd0e90dba1e34f6bd3699c237a5d90cbbc
Dermot <contact@dermotobrien.me>
2021-10-08 22:38:28 +0100

    Update minimum required App Version for iOS and Android

--- src/routes/Index.route.ts
+++ src/routes/Index.route.ts
@@ -1,8 +1,8 @@
 import express, { IRouter, Request, Response } from "express";
 var router: IRouter = express.Router();
 
-const IOS_MINIMUM_APP_VERSION = "1.24";
-const ANDROID_MINIMUM_APP_VERSION = "1.0.5";
+const IOS_MINIMUM_APP_VERSION = "2.04";
+const ANDROID_MINIMUM_APP_VERSION = "2.0.1";
 
 router.get('/', (req: Request, res: Response) => {
     res.status(200).json({ status: 200, message: "Fonzi API Server Active." })
