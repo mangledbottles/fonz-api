49452fed290c19b2c6f86c8c4c25178fe98a5329
Dermot <contact@dermotobrien.me>
2021-10-16 17:26:36 +0100

    Create Winston logger that syncs to AWS CloudWatch

--- src/config/Logger.ts
+++ src/config/Logger.ts
@@ -1,39 +1,39 @@
-const winston = require('winston'),
-    CloudWatchTransport = require('winston-aws-cloudwatch');
+var winston = require('winston');
+const WinstonCloudWatch = require('winston-cloudwatch'),
+    crypto = require('crypto');
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+// Give ourselves a randomized (time-based) hash to append to our stream name
+// so multiple instances of the server running don't log to the same
+// date-separated stream.
+var startTime = new Date().toISOString();
 
-const logger = new winston.createLogger({
+winston.loggers.add('access-log', {
   transports: [
-    new (winston.transports.Console)({
-      timestamp: true,
+    new winston.transports.Console({
+      json: true,
       colorize: true,
+      level: 'info'
+    }),
+    new WinstonCloudWatch({
+      logGroupName: 'Fonz-API-Log',
+      logStreamName: function() {
+        // Spread log streams across dates as the server stays up
+        let date = new Date().toISOString().split('T')[0];
+        return 'Fonz-API-' + date + '-' +
+          crypto.createHash('md5')
+          .update(startTime)
+          .digest('hex');
+      },
+      awsRegion: 'eu-west-1',
+      jsonMessage: true,
+      awsOptions: {
+        accessKeyId: process.env.CLOUDWATCH_ACCESS_KEY_ID,
+        secretAccessKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
+      },
     })
   ]
 });
 
-var config = {
-  logGroupName: 'API-Cloudwatch-Logger',
-  logStreamName: NODE_ENV,
-  createLogGroup: false,
-  createLogStream: true,
-  awsConfig: {
-    accessKeyId: process.env.CLOUDWATCH_ACCESS_KEY_ID,
-    secretAccessKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
-  },
-  formatLog: function (item) {
-    return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
-  }
-}
+const logg = winston.loggers.get('access-log');
 
-if (NODE_ENV == 'development') logger.add(CloudWatchTransport, config);
-
-logger.level = process.env.LOG_LEVEL || "silly";
-
-logger.stream = {
-  write: function(message, encoding) {
-    logger.info(message);
-  }
-};
-
-module.exports = logger;
+module.exports = logg;
