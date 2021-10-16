c04409e2517ec636c45febab1664e2b8a883f95a
Dermot <contact@dermotobrien.me>
2021-10-16 17:30:18 +0100

    Add Winston Logger to all User Routes

--- src/routes/User.route.ts
+++ src/routes/User.route.ts
@@ -1,26 +1,33 @@
 import express, { IRouter, Request, Response } from "express";
 var router: IRouter = express.Router();
+const NAMESPACE = "User";
 
 /** Import controllers */
 const User = require('../controller/User.controller');
 
 router.put('/', async (req: Request, res: Response) => {
     try {
+        globalThis.Logger.log('info', `[${NAMESPACE}] Updating User Account `, { ...globalThis.LoggingParams})
+        
         const { displayName, email, password, agreedMarketing, agreedConsent } = req.body;
         const user = await User.updateAccount(email, displayName, password, agreedMarketing, agreedConsent);
         res.send(user);
     } catch(error) {
-        console.error(error);
+        globalThis.Logger.log('error', `[${NAMESPACE}] Could not Update User Account`, { ...globalThis.LoggingParams, error },)
+
         res.status(error.status || 500).send(error);
     }
 })
 
 router.get('/', async (req: Request, res: Response) => {
     try {
+        globalThis.Logger.log('info', `[${NAMESPACE}] Get User Account `, { ...globalThis.LoggingParams})
+
         const user = await User.getAccount();
         res.send(user);
     } catch(error) {
-        console.error(error);
+        globalThis.Logger.log('error', `[${NAMESPACE}] Could not Get User Account`, { ...globalThis.LoggingParams, error },)
+
         res.status(error.status || 500).send(error);
     }
 })
