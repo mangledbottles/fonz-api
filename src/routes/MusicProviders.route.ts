e000099407eb0b64338dedf9dcff5f04a867b406
Dermot <contact@dermotobrien.me>
2021-10-16 17:29:57 +0100

    Add Winston Logger to all Music Provider Routes

--- src/routes/MusicProviders.route.ts
+++ src/routes/MusicProviders.route.ts
@@ -1,5 +1,6 @@
 import express, { IRouter, Request, Response } from "express";
 var router: IRouter = express.Router();
+const NAMESPACE = "MusicProviders"
 
 /** Import dependecies */
 var SpotifyWebApi = require('spotify-web-api-node');
@@ -8,16 +9,21 @@ var SpotifyWebApi = require('spotify-web-api-node');
 const Host = require('../controller/Host.controller');
 
 router.get('/', async (req: Request, res: Response) => {
+    globalThis.Logger.log('info', `[${NAMESPACE}] User getting their Music Providers `, { ...globalThis.LoggingParams })
+
     try {
         const musicProviders = await Host.getMusicProviders();
         res.send(musicProviders);
     } catch (error) {
-        console.error(error);
+        globalThis.Logger.log('error', `[${NAMESPACE}] Could not Get users Music Providers`, { ...globalThis.LoggingParams, error },)
+
         res.status(error.status || 500).send(error);
     }
 })
 
 router.get('/spotify', async (req: Request, res: Response) => {
+    globalThis.Logger.log('info', `[${NAMESPACE}] User requesting auth link to add Spotify `, { ...globalThis.LoggingParams })
+
     try {
         const { device } = req.query;
         if(!device) res.status(403).send({ message: "Missing device parameter"});
@@ -31,7 +37,8 @@ router.get('/spotify', async (req: Request, res: Response) => {
         const authorizeURL = spotifyApi.createAuthorizeURL(scopes, `${res.locals.userId}+${device}`);
         res.send({ authorizeURL })
     } catch (error) {
-        console.error(error);
+        globalThis.Logger.log('error', `[${NAMESPACE}] Could not Get auth link to add Spotify `, { ...globalThis.LoggingParams, error },)
+
         res.status(error.status || 500).send(error);
     }
 });
