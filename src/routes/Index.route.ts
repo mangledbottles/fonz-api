630bf5a55619c0c915b9687c80e7ee3fb9c36502
Dermot <contact@dermotobrien.me>
2021-10-16 17:29:49 +0100

    Add Winston Logger to all Index Routes

--- src/routes/Index.route.ts
+++ src/routes/Index.route.ts
@@ -1,16 +1,21 @@
 import express, { IRouter, Request, Response } from "express";
 var router: IRouter = express.Router();
+const NAMESPACE = "Index";
 
 const IOS_MINIMUM_APP_VERSION = "1.24";
 const ANDROID_MINIMUM_APP_VERSION = "1.0.5";
 
 router.get('/', (req: Request, res: Response) => {
+    globalThis.Logger.log('info', `[${NAMESPACE}] User checking Fonz Server Status `, { ...globalThis.LoggingParams })
+
     res.status(200).json({ status: 200, message: "Fonzi API Server Active." })
 });
 
 router.get('/version', (req: Request, res: Response) => {
     const { device } = req.query;
     if (!device) return res.status(400).json({ message: "Invalid device provided" });
+
+    globalThis.Logger.log('info', `[${NAMESPACE}] User checking their app version against minimum `, { ...globalThis.LoggingParams, params: { device }})
 
     const minimumAppVersion: String = (device == "iOS") ? IOS_MINIMUM_APP_VERSION : ANDROID_MINIMUM_APP_VERSION;
 
