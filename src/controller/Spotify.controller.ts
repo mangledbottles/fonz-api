dbc5f7d15ccc912cc9fd3c5136d5d21b67d82d0e
Dermot <contact@dermotobrien.me>
2021-11-11 15:54:05 +0000

    Remove stringification of JSON
    Data type updated in SQL

--- src/controller/Spotify.controller.ts
+++ src/controller/Spotify.controller.ts
@@ -111,7 +111,7 @@ exports.storeSpotifyCredentials = ({ email, display_name, product, country, spot
       const connection = await connect();
       const repo = connection.getRepository(MusicProviders);
 
-      const additional = JSON.stringify({ product, country, spotifyId, email, display_name });
+      const additional = { product, country, spotifyId, email, display_name };
       repo.save({ userId, country, expiresIn: expires_in, accessToken: access_token, refreshToken: refresh_token, additional, displayName: display_name })
 
       resolve({})
